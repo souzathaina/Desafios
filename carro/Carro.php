2c8918f6957d9dd53d4e77a0e07239bea3efce9a
souzathaina <thainatavares.contato@gmail.com>
2025-06-10 21:17:44 -0300

    carro pronto

---
+++ carro/Carro.php
@@ -0,0 +1,71 @@
+<?php
+class Carro
+{
+    private string $modelo;
+    private bool $combustivel;
+    private float $tanque;
+    private int $consumo;
+    private int $kmRodado;
+
+    public function __construct(string $modelo, bool $combustivel, $tanque, int $consumo, int $kmRodado)
+    {
+        $this->modelo = $modelo;
+        $this->combustivel = $combustivel;
+        $this->tanque = $tanque;
+        $this->consumo = $consumo;
+        $this->kmRodado = $kmRodado;
+    }
+
+    public function getModelo(): string
+    {
+        return $this->modelo;
+    }
+
+    public function getCombustivel()
+    {
+        return $this->combustivel;
+    }
+
+    public function getTanque()
+    {
+        return $this->tanque;
+    }
+
+    public function getConsumo()
+    {
+        return $this->consumo;
+    }
+
+    public function calcularAutonomia()
+    {
+        return $this->tanque * $this->consumo;
+    }
+
+    public function calcularCustoPorKm($precoCombustivel)
+    {
+        return $precoCombustivel / $this->consumo;
+    }
+
+    public function verificarRevisao()
+    {
+        if ($this->kmRodado >= 1000) {
+            return "Precisa realizar";
+        } else {
+            return "Não precisa realizar;";
+        }
+    }
+    public function exibirDetalhes($precoCombustivel): string
+    {
+        return "
+        <ul>
+            <li>Modelo: {$this->modelo}</li>
+            <li>Autonomia: {$this->calcularAutonomia()} km</li>
+            <li>Custo por km: R$ " . number_format($this->calcularCustoPorKm($precoCombustivel), 2, ',', '.') . "</li>
+            <li>Revisão: {$this->verificarRevisao()}</li>
+        </ul>";
+    }
+}
+
+
+
+?>
---
+++ carro/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pedido</title>
+</head>
+
+<body>
+    <h1>Formulário de Pedido</h1>
+    <form method="POST">
+        <label>Modelo:</label>
+        <input type="text" name="modelo" required><br><br>
+
+        <label>Tipo de Combustivel:</label>
+        <select name="combustivel">
+            <option value="normal">Gasolina</option>
+            <option value="premium">Etanol</option>
+        </select><br><br>
+
+        <label>Consumo (Km/l):</label>
+        <input type="number" name="consumo" required><br><br>
+
+        <label>Valor do Combustivel:</label>
+        <input type="number" step="0.01" name="precoCombustivel" required><br><br>
+
+        <label>Kilometros rodados:</label>
+        <input type="number" name="kmRodado" required><br><br>
+
+        <label>Capacidade do Tanque (litros):</label>
+        <input type="number" step="0.01" name="tanque" required><br><br>
+
+        <button type="submit">Verificar</button>
+    </form>
+
+    <?php
+    require_once "Carro.php";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $modelo = $_POST["modelo"];
+        $combustivel = ($_POST["combustivel"] === "gasolina");
+        $tanque = (int) $_POST["tanque"];
+        $consumo = (float) $_POST["consumo"];
+        $kmRodado = (float) $_POST["kmRodado"];
+        $precoCombustivel = (float) $_POST["precoCombustivel"];
+
+        $pedido = new Carro($modelo, $combustivel, $tanque, $consumo, $kmRodado);
+        echo "<h3>Resultado:</h3>";
+        echo $pedido->exibirDetalhes($precoCombustivel);
+    }
+    ?>
+</body>
+
+</html>
